5395b066daf1c7702a4289f76289c3e2947190d9
alexk1923 <alexandrukullman@gmail.com>
2023-05-28 15:16:37 +0300

    WORKING NAVIGATION, without playing

--- piano_board.ino
+++ piano_board.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 #include "pitches.h"
-
+#include "PCF8574.h"
+#include "sdios.h"
 /* 
 This example demonstrates how to use the SDFAT library for audio playback.
 Read time and program space are reduced by using SDFAT directly
@@ -32,16 +33,18 @@ TMRpcm audio;  // create an object for use in this sketch
 #else  // HAS_SDIO_CLASS
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
 #endif  // HAS_SDIO_CLASS
+
+#define error(s) sd.errorHalt(&Serial, F(s))
 //------------------------------------------------------------------------------
 
-#define BUTTON_1 PD2
+#define BUTTON_1 PD7
 #define BUTTON_2 PD3
 #define BUTTON_3 PD5
 #define BUTTON_4 PD6
-#define MODE_BUTTON PD7
-#define ENTER_BUTTON PB2
-#define RETURN_BUTTON PB0
-
+#define MODE_BUTTON P0
+#define ENTER_BUTTON P1
+#define RETURN_BUTTON P2
+#define ARDUINO_UNO_INTERRUPTED_PIN 2
 
 #define SPEAKER PB1
 #define LED_1 A0
@@ -49,15 +52,23 @@ TMRpcm audio;  // create an object for use in this sketch
 #define LED_3 A2
 #define LED_4 A3
 
-#define FREE_MODE "FREE"
-#define RECORD_MODE "RECORD"
-#define LEARN_MODE "LEARN"
-#define LISTEN_MODE "LISTEN"
+// #define FREE_MODE "FREE"
+// #define RECORD_MODE "RECORD"
+// #define LEARN_MODE "LEARN"
+// #define LISTEN_MODE "LISTEN"
 
 File root;
 File myFile;
+File file;
 
+// Function interrupt
+void keyChangedOnPCF8574();
+
+
+PCF8574 pcf8574(0x20, ARDUINO_UNO_INTERRUPTED_PIN, keyChangedOnPCF8574);
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C address 0x27, 16 column and 2 rows
+// char currentFile[15] = "";
+
 
 volatile long timeFromPress = millis();
 long debounceTime = 150;
@@ -74,31 +85,15 @@ volatile bool changeMode = true;
 volatile bool recordingToFile = false;
 volatile bool learning = false;
 volatile bool listenSong = false;
-int learningNotes[25] = { 0 };
+int learningNotes[2] = { 0 };
 volatile int learningNotesSize = 0;
 volatile int currentLearningNotesIdx = -1;
 volatile bool currentLearningNoteChange = false;
+volatile bool pcf8574_interrupt = false;
+volatile bool navigate = false;
 
-ISR(INT0_vect) {
-  // cod întrerupere externă
-  //  PORTD ^= (1 << PD7);
-  long actTime = millis();
-  if (actTime - timeFromPress > debounceTime) {
-    timeFromPress = actTime;
-    Serial.println("Intrerupere externa");
-  }
-}
+int currentFileIndex = 1;
 
-ISR(PCINT0_vect) {
-  long actTime = millis();
-
-  if (actTime - timeFromPress > debounceTime) {
-    if ((PINB & (1 << ENTER_BUTTON)) == 0) {
-      Serial.println("ENTER");
-    }
-    timeFromPress = actTime;
-  }
-}
 
 ISR(PCINT2_vect) {
   // cod întrerupere de tip pin change
@@ -116,58 +111,7 @@ ISR(PCINT2_vect) {
   long actTime = millis();
 
   if (actTime - timeFromPress > debounceTime) {
-    if ((PIND & (1 << MODE_BUTTON)) == 0) {
-      currentModeIdx = (currentModeIdx + 1) % 4;
-      changeMode = true;
 
-      if(currentModeIdx == 0) {
-        audio.stopPlayback();
-      }
-
-
-      if (currentModeIdx == 1) {
-        if (recordingToFile) {
-          // Serial.println("Closing file");
-          // myFile.close();
-        } else {
-          //  Serial.println("Setting recordingToFile to true");
-          recordingToFile = true;
-        }
-      }
-
-      if (currentModeIdx == 2) {
-        // Open the file for reading
-        int statusOpen = myFile.open("data52.txt", O_READ);
-        if (statusOpen) {
-          // Read and print each line from the file
-          learningNotesSize = 0;
-          while (myFile.available() && learningNotesSize < 25) {
-            String line = myFile.readStringUntil('\n');
-            learningNotes[learningNotesSize++] = line.toInt();
-          }
-          myFile.close();
-
-          if (learningNotesSize > 0) {
-
-            Serial.println("My notes:");
-            for (int i = 0; i < learningNotesSize; i++) {
-              Serial.println(learningNotes[i]);
-            }
-
-
-            learning = true;
-            currentLearningNotesIdx = 0;
-            currentLearningNoteChange = true;
-          }
-        } else {
-          Serial.println("Error opening file for reading!");
-        }
-      }
-
-      if(currentModeIdx == 3) {
-        listenSong = true;
-      }
-    }
 
     /*
     if ((PIND & (1 << ENTER_BUTTON)) == 0) {
@@ -306,7 +250,7 @@ void setup_interrupts() {
 
 
   //PCMSK2 |= (1 << PCINT16); // Turns on PCINT18 (PD2)
-  PCMSK2 |= (1 << PCINT18);  // Turns on PCINT18 (PD2)
+  // PCMSK2 |= (1 << PCINT18);  // Turns on PCINT18 (PD2)
   PCMSK2 |= (1 << PCINT19);  // Turns on PCINT19 (PD3)
   PCMSK2 |= (1 << PCINT21);  // Turns on PCINT21 (PD5)
   PCMSK2 |= (1 << PCINT22);  // Turns on PCINT22 (PD6)
@@ -329,38 +273,38 @@ void setup_leds() {
 }
 
 void setup_buttons() {
+
+  pcf8574.pinMode(P0, INPUT_PULLUP, HIGH);
+  pcf8574.pinMode(P1, INPUT_PULLUP, HIGH);
+  pcf8574.pinMode(P2, INPUT_PULLUP, HIGH);
+  pcf8574.pinMode(P3, INPUT_PULLUP, HIGH);
+
   for (int i = 0; i < 4; i++) {
     // Pin Mode Input Pullup
     DDRD &= ~(1 << buttons[i]);
     // Set default button state to high
     PORTD |= (1 << buttons[i]);
   }
-
-  // Pin Mode Input Pullup
-  DDRD &= ~(1 << MODE_BUTTON);
-  // Set default button state to high
-  PORTD |= (1 << MODE_BUTTON);
-
-  // Pin Mode Input Pullup
-  DDRB &= ~(1 << ENTER_BUTTON);
-  // Set default button state to high
-  PORTB |= (1 << ENTER_BUTTON);
-
-  // Pin Mode Input Pullup
-  DDRB &= ~(1 << RETURN_BUTTON);
-  // Set default button state to high
-  PORTB |= (1 << RETURN_BUTTON);
 }
 
 void setup() {
 
-
+  lcd.clear();
   lcd.print("Starting piano...");
   Serial.begin(9600);
 
   setup_lcd();
   setup_buttons();
   setup_interrupts();
+
+
+
+  Serial.print("Init pcf8574...");
+  if (pcf8574.begin()) {
+    Serial.println("OK");
+  } else {
+    Serial.println("KO");
+  }
 
   // Check for SPI Communication with SD Card
   if (!sd.begin(4, SPI_FULL_SPEED)) {
@@ -373,35 +317,139 @@ void setup() {
   // pinMode(10,OUTPUT); //Pin pairs: 9,10 Mega: 5-2,6-7,11-12,46-45
 
   Serial.println("done!");
-
   audio.setVolume(5);
- // audio.play("3.wav");
-
-  /*
-  // Navigate in SD Card
-  File dir = sd.open("/");
-  while (true) {
-    File file = dir.openNextFile(); 
-    if (!file) {
-      // no more files
-      break;
-    }
-
-    char filename[13];
-    file.getName(filename, sizeof(filename));
-    lcd.print(filename);
-    delay(5000);
-    lcd.clear();
-    Serial.println(filename);
-    //Serial.print("Size: ");
-    //Serial.println(file.size());
-    file.close();
-  }*/
+  // audio.play("3.wav");
 
   lcd.print("Done!");
   delay(2000);
   lcd.clear();
   lcd.print(modes[currentModeIdx]);
+}
+
+void resetPCF(bool val0, bool val1, bool val2, bool val3) {
+  if (val0 == HIGH) {
+    pcf8574.digitalWrite(P0, LOW);
+  } else {
+    pcf8574.digitalWrite(P0, HIGH);
+  }
+
+  if (val1 == HIGH) {
+    pcf8574.digitalWrite(P1, LOW);
+  } else {
+    pcf8574.digitalWrite(P1, HIGH);
+  }
+
+  if (val2 == HIGH) {
+    pcf8574.digitalWrite(P2, LOW);
+  } else {
+    pcf8574.digitalWrite(P2, HIGH);
+  }
+
+  if (val3 == HIGH) {
+    pcf8574.digitalWrite(P3, LOW);
+  } else {
+    pcf8574.digitalWrite(P3, HIGH);
+  }
+}
+
+
+void mode_change() {
+
+  currentModeIdx = (currentModeIdx + 1) % 4;
+  changeMode = true;
+
+  if (currentModeIdx == 0) {
+    audio.stopPlayback();
+  }
+
+
+  if (currentModeIdx == 1) {
+    if (recordingToFile) {
+    } else {
+      // recordingToFile = true;
+    }
+  }
+
+  if (currentModeIdx == 2) {
+    // Open the file for reading
+    // int statusOpen = myFile.open("data52.txt", O_READ);
+    // if (statusOpen) {
+    //   // Read and print each line from the file
+    //   learningNotesSize = 0;
+    //   while (myFile.available() && learningNotesSize < 25) {
+    //     String line = myFile.readStringUntil('\n');
+    //     learningNotes[learningNotesSize++] = line.toInt();
+    //   }
+    //   myFile.close();
+
+    //   if (learningNotesSize > 0) {
+
+    //     Serial.println("My notes:");
+    //     for (int i = 0; i < learningNotesSize; i++) {
+    //       Serial.println(learningNotes[i]);
+    //     }
+
+
+    //     learning = true;
+    //     currentLearningNotesIdx = 0;
+    //     currentLearningNoteChange = true;
+    //   }
+    // } else {
+    //   Serial.println("Error opening file for reading!");
+    // }
+  }
+
+  if (currentModeIdx == 3) {
+    // listenSong = true;
+  }
+}
+
+void enter_press() {
+  switch (currentModeIdx) {
+    // FREE
+    case 0:
+      Serial.println("Does nothing, it is in free mode");
+      break;
+    // RECORD
+    case 1:
+      Serial.println("Setting record mode to true");
+      learning = false;
+      listenSong = false;
+      recordingToFile = true;
+      break;
+    // LEARN
+    case 2:
+      //Serial.println("Setting learning mode to true");
+      delay(100);
+      recordingToFile = false;
+      listenSong = false;
+      learning = true;
+      break;
+    // LISTEN
+    case 3:
+      // @TODO, navigate in the SD Card to play a song or back to mode select
+      // Serial.println("Setting listen mode to true");
+      navigate = true;
+      learning = false;
+      recordingToFile = false;
+      listenSong = true;
+      break;
+    default:
+      Serial.println("Index error");
+      break;
+  }
+}
+
+void right_button() {
+  if (navigate) {
+    navigate_sd(1);
+  }
+}
+
+void left_button() {
+  if (navigate) {
+    navigate_sd(-1);
+  }
 }
 
 void loop() {
@@ -437,10 +485,40 @@ void loop() {
     currentLearningNoteChange = false;
   }
 
-  if(listenSong) {
-    audio.play("trot.wav");
+
+  if (listenSong) {
+    navigate_sd(-1);
+    //audio.play("5.wav");
     listenSong = false;
   }
+
+  if (pcf8574_interrupt) {
+    bool val0 = pcf8574.digitalRead(P0);
+    bool val1 = pcf8574.digitalRead(P1);
+    bool val2 = pcf8574.digitalRead(P2);
+    bool val3 = pcf8574.digitalRead(P3);
+
+
+    if (val0 == LOW) {
+      mode_change();
+    }
+
+    if (val1 == LOW) {
+      enter_press();
+    }
+
+    if (val2 == LOW) {
+      left_button();
+    }
+
+    if (val3 == LOW) {
+      right_button();
+    }
+
+    resetPCF(val0, val1, val2, val3);
+    pcf8574_interrupt = false;
+  }
+
 
   if (Serial.available()) {
     switch (Serial.read()) {
@@ -449,4 +527,112 @@ void loop() {
       default: break;
     }
   }
+}
+
+
+
+void keyChangedOnPCF8574() {
+
+  // bool val1 = pcf8574.digitalRead(P1);
+  // bool val2 = pcf8574.digitalRead(P2);
+  long actTime = millis();
+  if (actTime - timeFromPress > debounceTime) {
+    //uint8_t val0 = pcf8574.digitalRead(P0);
+    pcf8574_interrupt = true;
+  }
+}
+
+
+void navigate_sd(int direction) {
+  currentFileIndex += direction;
+  // Navigate in SD Card
+
+  if (currentFileIndex < 0) {
+    currentFileIndex = 0;
+  }
+
+
+  if (listenSong == true) {
+    // dir = sd.open("/original", O_READ);
+  }
+
+  if (recordingToFile == true) {
+    // dir = sd.open("/original", O_READ);
+  }
+
+  Serial.println(currentFileIndex);
+
+
+  char fileNames[15];
+  int rootFileCount = 0;
+  if (!root.open("/original")) {
+    error("open root");
+  }
+  // strcpy(fileNames[0], "ddd");
+  int noFiles = 0;
+  bool foundFile = false;
+
+  while (!foundFile && file.openNext(&root, O_RDONLY)) {
+    Serial.println("here");
+      file.getName(fileNames, sizeof(fileNames));
+      if(noFiles == currentFileIndex) {
+        // currentFile = fileNames;
+        // strncat(currentFile, fileNames, 3);
+        lcd.clear();
+        lcd.print(fileNames);
+
+        // currentFile[i] = '\0';
+        foundFile = true;
+      }
+      noFiles++;
+    
+    file.close();
+    if (rootFileCount > 10) {
+      error("Too many files in root. Please use an empty SD.");
+    }
+  }
+
+  root.close();
+
+  // Adjust the current file index if it goes out of bounds
+  if (!foundFile) {
+    // strcpy(currentFile, fileNames);
+    lcd.clear();
+    lcd.print(fileNames);
+    currentFileIndex = noFiles - 1;
+  }
+
+
+  // Print the current file name
+
+
+  // Serial.print("Current File: ");
+  // Serial.println(fileNames[currentFileIndex]);
+
+  /*
+  if (currentFileIndex < 0) {
+    currentFileIndex = noFiles - 1;
+  } else if (currentFileIndex >= noFiles) {
+    currentFileIndex = 0;
+  }
+
+  Serial.print("Current File: ");
+  Serial.println(files[currentFileIndex]);*/
+  // while (true) {
+  //   File file = dir.openNextFile();
+  //   if (!file) {
+  //     // no more files
+  //     break;
+  //   }
+
+  //   char filename[13];
+  //   file.getName(filename, sizeof(filename));
+  //   lcd.print(filename);
+  //   delay(5000);
+  //   lcd.clear();
+  //   Serial.println(filename);
+  //   //Serial.print("Size: ");
+  //   //Serial.println(file.size());
+  //   file.close();
+  // }
 }
